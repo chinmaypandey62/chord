1249e261cd1fdc9fd599bac3517d1a72e79f71fa
CHINMAY PANDEY <108930086+chinmaypandey62@users.noreply.github.com>
2025-04-27 17:37:24 +0000

    updated the cors config

--- server/config/corsConfig.js
+++ server/config/corsConfig.js
@@ -1,6 +1,6 @@
 // CORS configuration
 const corsConfig = {
-  origin: ['https://chord.onrender.com', 'http://localhost:5000'],
+  origin: [process.env.CLIENT_URI, process.env.LOCAL_CLIENT_URI],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
