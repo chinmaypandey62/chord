fc16cbfee4fba11e5b0770dcbae0a05a07496e66
CHINMAY PANDEY <108930086+chinmaypandey62@users.noreply.github.com>
2025-04-29 17:06:35 +0000

    Update CORS configuration to allow all origins

--- server/config/corsConfig.js
+++ server/config/corsConfig.js
@@ -1,6 +1,6 @@
 // CORS configuration
 const corsConfig = {
-  origin: ["https://chord-app.onrender.com", process.env.CLIENT_URI_LOCAL],
+  origin: "*",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
